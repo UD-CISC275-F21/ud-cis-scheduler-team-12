409f62ae1b6d917e5ddc01ee5ee291c5ec926fdd
srinathv31 <48702600+srinathv31@users.noreply.github.com>
2021-10-12 22:06:26 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
+                <p>
+                     Edit <code>src/App.tsx</code> and save to reload.
+                 </p>
                 <h1>CISC 275 Final Project</h1>
                 <p>Srinath Venkatesh</p>
             </header>
