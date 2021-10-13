411f0d80408afe0afd8defdc0089e61e87dc7319
Srinath Venkatesh <srinathvb1@gmail.com>
2021-10-13 12:28:08 -0400

    10/13 spaces fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App(): JSX.Element {
         <div className="App">
             <header className="App-header">
                 <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                 </p>
+                     Edit <code>src/App.tsx</code> and save to reload.
+                </p>
                 <h1>CISC 275 Final Project</h1>
                 <p>Srinath Venkatesh</p>
             </header>
