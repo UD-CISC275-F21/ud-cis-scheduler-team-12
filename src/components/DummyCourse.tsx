dbe2f86a881b4a5eaa224e5bea18d782ccc235a1
glindy <glindy@udel.edu>
2021-10-21 03:03:02 -0400

    updated css rules for board and course cards

--- src/components/DummyCourse.tsx
+++ src/components/DummyCourse.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+/* Placeholder for a real Course object to be placed on Board */
+
 export function DummyCourse({ value, onClick }) {
     return (
-        <button className="course" onClick={onClick}>
+        <button className="course-card" onClick={onClick}>
             {value}
         </button>
     );
