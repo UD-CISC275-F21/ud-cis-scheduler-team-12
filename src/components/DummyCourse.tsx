c16f8ef404d81e2b661687dd242371f5e4af230e
Elliot <elliottingey@yahoo.com>
2021-10-24 18:06:45 -0400

    line updates and renaming

--- src/components/DummyCourse.tsx
+++ src/components/DummyCourse.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-Placeholder for a real Course object to be placed on Board
+//Placeholder for a real Course object to be placed on Board
 
 export function DummyCourse({ value, onClick }) {
     return (
