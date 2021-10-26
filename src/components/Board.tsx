a564fb3b6a1be0061a72a4a73b68986ec2522995
glindy <glindy@udel.edu>
2021-10-26 04:27:49 -0400

    iterating on Board to display all courses, WIP

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,45 +1,42 @@
-import React from "react";
-// import { Col, Row, Container} from "react-bootstrap";
-// import CourseComp from "./Courses";
+import React, { useState } from "react";
+import { Course } from "../interfaces/course";
+import { Col, Row, Container, Card } from "react-bootstrap";
+import CourseComp from "./Courses";
+import ClassList from "../assets/courses.json";
 
 
-export function Board(): JSX.Element {
-    // const [courses, setCourses] = useState(Array(9).fill(null));
+// export function Board():  JSX.Element {
+//     const [courses, setCourses] = useState(Array(9).fill(null));
 
-    // function renderCourse(i: number): JSX.Element {
-    //     return (
-    //         <CourseComp
-    //             value={courses[i]}
-    //             onClick={() => {
-    //                 return;
-    //             }}
-    //         />
-    //     );
-    // }
+//     function renderCourse({ i }: { i: number }):  JSX.Element {
+//         return (
+//             <CourseComp
+//                 course1={ClassList[i]}
+//             ></CourseComp>
+//         );
+//     }
 
-    return (
-        <div>text</div>
-        // <Container>
-        //     <Row className="courses">
-        //         <Col className="course-board">
-        //             <div className="label-board"><h4>Courses</h4></div>
-        //             <div className="board-row">
-        //                 {renderCourse(0)}
-        //                 {renderCourse(1)}
-        //                 {renderCourse(2)}
-        //             </div>
-        //             <div className="board-row"> 
-        //                 {renderCourse(3)}
-        //                 {renderCourse(4)}
-        //                 {renderCourse(5)}
-        //             </div>
-        //             <div className="board-row">
-        //                 {renderCourse(6)}
-        //                 {renderCourse(7)}
-        //                 {renderCourse(8)}
-        //             </div>
-        //         </Col>
-        //     </Row>
-        // </Container>
-    );
-}
+//     return (
+//         <div>
+//             <Container>
+//                 <Row className="courses">
+//                     <Col className="course-board">
+//                         <div className="label-board"><h4>Courses</h4></div>
+//                         <div className="board-row">
+//                             {renderCourse(0)}
+//                             {renderCourse(1)}
+//                             {renderCourse(2)}
+//                         </div>
+//                         <div className="board-row"> 
+//                             {renderCourse(3)}
+//                             {renderCourse(4)}
+//                             {renderCourse(5)}
+//                         </div>
+//                         <div className="board-row">
+//                         </div>
+//                     </Col>
+//                 </Row>
+//             </Container>
+//         </div>
+//     );
+// }
