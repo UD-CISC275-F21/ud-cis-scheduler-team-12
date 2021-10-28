1928e2cf18d98bed9ac5da7a1df914b1b3523996
glindy <glindy@udel.edu>
2021-10-27 21:54:02 -0400

    arranged board 3x2, removed DummyCourse.tsx

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Col, Row, Container } from "react-bootstrap";
 import CourseComp from "./CourseComp";
 import ClassList from "../assets/courses.js";
+import "../css/board.css";
 
 
 export function Board():  JSX.Element {
@@ -19,9 +20,9 @@ export function Board():  JSX.Element {
     return (
         <div>
             <h2>Courses</h2>
-            <Container>
-                <Row className="course-row-1">
-                    <Col>
+            <Container id="board">
+                <Row className="board-row" id="board-row-1">
+                    <Col className="board-col">
                         {/* {renderCourse(0)}
                         {renderCourse(1)}
                         {renderCourse(2)} */}
@@ -29,29 +30,29 @@ export function Board():  JSX.Element {
                             course1={ClassList[0]}
                         ></CourseComp>
                     </Col>
-                    <Col>
+                    <Col className="board-col">
                         <CourseComp
                             course1={ClassList[1]}
                         ></CourseComp>
                     </Col>
-                    <Col>
+                    <Col className="board-col">
                         <CourseComp
                             course1={ClassList[2]}
                         ></CourseComp>
                     </Col>
                 </Row>
-                <Row>
-                    <Col>
+                <Row className="board-row" id="board-row-1">
+                    <Col className="board-col">
                         <CourseComp
                             course1={ClassList[3]}
                         ></CourseComp>
                     </Col>
-                    <Col>
+                    <Col className="board-col">
                         <CourseComp
                             course1={ClassList[4]}
                         ></CourseComp>
                     </Col>
-                    <Col>
+                    <Col className="board-col">
                         <CourseComp
                             course1={ClassList[5]}
                         ></CourseComp>
