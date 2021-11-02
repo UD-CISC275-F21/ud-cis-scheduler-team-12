1bba0293a37afb1ef3ab1ab1072a5d71676a4bdb
Elliot <elliottingey@yahoo.com>
2021-11-02 02:54:22 -0400

    fixes for the linter

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-//import { Col, Row, Container} from "react-bootstrap";
+import {Container} from "react-bootstrap";
 import "../css/calender.css";
 //import { Weekday, Date } from "../interfaces/weekday";
 import Weekdays from "../assets/weekdays";
