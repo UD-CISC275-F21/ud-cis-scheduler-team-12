106990931487967ecd23a16febd2b151750dd120
Srinath Venkatesh <srinathvb1@gmail.com>
2021-11-07 12:17:24 -0500

    11/7 remove class on drag/card shadows added

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,8 +4,10 @@ import CourseComp from "./CourseComp";
 import ClearSemesterButton from "./ClearSemesterButton";
 import "../css/board.css";
 import { Course } from "../interfaces/course";
+import courseData from "../assets/courses";
 
 import buttonList from "../assets/buttonList";
+import { AnimatePresence, motion } from "framer-motion";
 
 export function Board({ setSemesterSelect, semesterSelect, SET_SEMESTER_MAP, SEMESTER_MAP, setSemesterHeader, semesterHeader }: {
     setSemesterSelect: (s: string | null) => void, semesterSelect: string | null,
@@ -17,6 +19,13 @@ export function Board({ setSemesterSelect, semesterSelect, SET_SEMESTER_MAP, SEM
     const classListToPrint = SEMESTER_MAP[""+semesterSelect];
 
     const buttonToggle = ""+semesterSelect;
+
+    function removeCourse(id: number) {
+        const NEW_SEMESTER_MAP = {...SEMESTER_MAP};
+        
+        NEW_SEMESTER_MAP[""+semesterSelect] = NEW_SEMESTER_MAP[""+semesterSelect].filter(item => item !== courseData[id]);
+        SET_SEMESTER_MAP(NEW_SEMESTER_MAP);
+    }
 
     function handleSelect (val: number) {
         setSemesterSelect(""+val);
@@ -46,16 +55,37 @@ export function Board({ setSemesterSelect, semesterSelect, SET_SEMESTER_MAP, SEM
             <div>
                 <Container>
                     <Row xs={1} md={3}>
-                        {classListToPrint.map(classListToPrint =>
-                            <Col key={classListToPrint.id}>
-                                <CourseComp
-                                    course={classListToPrint}
-                                    SET_SEMESTER_MAP={SET_SEMESTER_MAP}
-                                    SEMESTER_MAP={SEMESTER_MAP}
-                                    semesterSelect={semesterSelect}
-                                ></CourseComp>
-                            </Col>
-                        )}
+                        <AnimatePresence>
+                            {classListToPrint.map(classListToPrint =>
+                                <motion.div
+                                    key={classListToPrint.id}
+                                    drag
+                                    dragConstraints={{
+                                        top: 0,
+                                        bottom: 0,
+                                        right: 0,
+                                        left: 0
+                                    }}
+                                    onDragEnd={() => removeCourse(classListToPrint.id)}
+                                    dragElastic={1}
+                                    initial={{ opacity: 0, x: 180 }}
+                                    animate={{ opacity: 1, x: 0 }}
+                                    exit={{ opacity: 0, y: -200, x: 180 }}
+                                    transition={{
+                                        ease: "easeInOut",
+                                        duration: 0.4
+                                    }}>
+                                    <Col >
+                                        <CourseComp
+                                            course={classListToPrint}
+                                            SET_SEMESTER_MAP={SET_SEMESTER_MAP}
+                                            SEMESTER_MAP={SEMESTER_MAP}
+                                            semesterSelect={semesterSelect}
+                                        ></CourseComp>
+                                    </Col>
+                                </motion.div>
+                            )}
+                        </AnimatePresence>
                     </Row>
                 </Container>
                 { SEMESTER_MAP[""+semesterSelect].length > 0 && <ClearSemesterButton
