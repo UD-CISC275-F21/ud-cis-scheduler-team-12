913307b942507b46268efd0884fe09cf88dc84d5
Srinath Venkatesh <srinathvb1@gmail.com>
2021-11-10 22:30:25 -0500

    11/10 RemoveAllSemesters updates preReqs dynamically

--- src/components/CourseComp.tsx
+++ src/components/CourseComp.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card,Col, Row, Container, Accordion } from "react-bootstrap/";
+import { Card,Col, Row, Container, Accordion, OverlayTrigger, Tooltip, Popover } from "react-bootstrap/";
 import "../css/courses.css";
 import "../assets/courses";
 import { Course } from "../interfaces/course";
@@ -58,50 +58,61 @@ function CourseComp({ course, SET_SEMESTER_MAP, SEMESTER_MAP, semesterSelect }: 
         return course.preReqCheck;
     }
 
+    // function getMissingPreReq(course: Course) {
+    //     Object.keys(course.preReq).forEach(course => {
+            
+    //     });
+    // }
+
 
     return (
         <div>           
-            <Card className="card" style={{ width: "19rem", color: updateColor(course) }}>
-                <Container>
-                    <Row>
-                        <Col>
-                            <Card.Title>{course.name}</Card.Title>
+            <OverlayTrigger trigger="hover" show={ Object.values(course.preReq).every(course => course === true) ? false : true } placement={ SEMESTER_MAP[""+semesterSelect].indexOf(course) > 2 ? "bottom" : "top" } overlay={
+                <Popover className="popover" id="tooltip-preReq">Missing: {Object.keys(course.preReq).filter(courseName => 
+                    course.preReq[courseName] === false).map(course => 
+                    <div key={course}>{course}</div>)} </Popover>}>
+                <Card className="card" style={{ width: "19rem", color: updateColor(course) }}>
+                    <Container>
+                        <Row>
+                            <Col>
+                                <Card.Title>{course.name}</Card.Title>
+                            </Col>
+                            <Col>
+                                <button className="delete-button" onClick={() => removeCourse(course.id)}>
+                                    <MdDeleteForever></MdDeleteForever></button>
+                            </Col>                        
+
+                        </Row>
+                    </Container>
+                    <Card.Body className="card-body">
+                        <Card.Text>
+                            Credits: {course.credits}
+                        </Card.Text>
+                        <Card.Text>
+                            From: {course.timeStart} To: {course.timeEnd}
+                        </Card.Text>
+
+                        <Card.Text>
+                            Days: {course.schedule}
+                        </Card.Text>
+                        <Col className="column-dropdown">
                         </Col>
-                        <Col>
-                            <button className="delete-button" onClick={() => removeCourse(course.id)}>
-                                <MdDeleteForever></MdDeleteForever></button>
-                        </Col>                        
-
-                    </Row>
-                </Container>
-                <Card.Body className="card-body">
-                    <Card.Text>
-                        Credits: {course.credits}
-                    </Card.Text>
-                    <Card.Text>
-                        From: {course.timeStart} To: {course.timeEnd}
-                    </Card.Text>
-
-                    <Card.Text>
-                        Days: {course.schedule}
-                    </Card.Text>
-                    <Col className="column-dropdown">
-                    </Col>
-                
-                    <Col className="card-accordion">
-                        <Accordion>
-                            <Accordion.Item eventKey="0">
-                                <Accordion.Header>Details</Accordion.Header>
-                                <Accordion.Body>
-                                    <button className="edit-button" onClick={() => editDescription()}>
-                                        <GrEdit></GrEdit></button>
-                                    Course description: {course.description}
-                                </Accordion.Body>
-                            </Accordion.Item>
-                        </Accordion>
-                    </Col>
-                </Card.Body>
-            </Card>
+                    
+                        <Col className="card-accordion">
+                            <Accordion>
+                                <Accordion.Item eventKey="0">
+                                    <Accordion.Header>Details</Accordion.Header>
+                                    <Accordion.Body>
+                                        <button className="edit-button" onClick={() => editDescription()}>
+                                            <GrEdit></GrEdit></button>
+                                        Course description: {course.description}
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                            </Accordion>
+                        </Col>
+                    </Card.Body>
+                </Card>
+            </OverlayTrigger>
         </div>
     );
 }
