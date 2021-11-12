697bad3aa5af8f84714010b23f80a84b78a97bed
Srinath Venkatesh <srinathvb1@gmail.com>
2021-11-11 21:56:53 -0500

    11/11 initial walkthrough implementation

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -10,9 +10,39 @@ import Calender from "../components/Calender";
 import DegreePlan from "../components/DegreePlan";
 import SaveBin from "./SaveBin";
 
+import ReactJoyride from "react-joyride";
+
 function MainPage({ visibleView, setVisibleView }: {
     setVisibleView: (s: string | null) => void, visibleView: string | null
 }): JSX.Element {
+
+    const state = {
+        steps: [
+            {
+                target: ".intro-step",
+                disableBeacon: true,
+                title: "Welcome!",
+                content: "Welcome to your personal Course Scheduler! To take the tour of the features, hit 'next'. Or you can skip the tour by hitting 'skip'.",
+            },
+            {
+                target: ".semester-step",
+                title: "Semester View",
+                content: "This is my awesome feature!",
+            },
+            {
+                target: ".menu-step",
+                title: "Menu",
+                content: "This is my awesome feature!",
+            },
+            {
+                target: ".right-menu-step",
+                title: "Search Bar/Bin",
+                content: "This another awesome feature!",
+            },
+        ]
+    };
+
+
     const [semesterSelect, setSemesterSelect] = useState<string | null>("1");
     const [semesterHeader, setSemesterHeader] = useState<string>("Fall 1");
 
@@ -37,28 +67,55 @@ function MainPage({ visibleView, setVisibleView }: {
 
     return (
         <div className="App">
+            <ReactJoyride
+                steps={state.steps}
+                continuous
+                showProgress
+                showSkipButton
+                disableOverlayClose
+                spotlightClicks
+                styles={{
+                    options: {
+                        arrowColor: "white",
+                        backgroundColor: "white",
+                        overlayColor: "rgba(0,0,0,0.6)",
+                        primaryColor: "#000",
+                        textColor: "black",
+                        width: 900,
+                        zIndex: 1000,
+                    }
+                }}
+            />
             <header className="App-header">
-                UD CIS Scheduler
-                <p>Srinath Venkatesh, Elliot Tingey, Geoffrey Linderman</p>
+                <div className="intro-step">
+                    UD CIS Scheduler
+                    <p>Srinath Venkatesh, Elliot Tingey, Geoffrey Linderman</p>
+                </div>
             </header>
             <section className="cell-left">
-                <p>Menu</p>
-                <SideMenu
-                    setVisibleView={setVisibleView}
-                ></SideMenu>
+                <div className="menu-step">
+                    <p>Menu</p>
+                    <SideMenu
+                        setVisibleView={setVisibleView}
+                    ></SideMenu>
+                </div>
             </section>
             <section className="cell-main">
-                { visibleView === "2" && <Board
-                    semesterHeader={semesterHeader}
-                    setSemesterHeader={setSemesterHeader}
-                    setSemesterSelect={setSemesterSelect}
-                    semesterSelect={semesterSelect}
-                    SET_SEMESTER_MAP={SET_SEMESTER_MAP}
-                    SEMESTER_MAP={SEMESTER_MAP}
-                    SET_SAVE_BIN={SET_SAVE_BIN}
-                    SAVE_BIN={SAVE_BIN}
-                    binVisible={binVisible}
-                ></Board> }
+                
+                <div className="semester-step">
+                    { visibleView === "2" && <Board
+                        semesterHeader={semesterHeader}
+                        setSemesterHeader={setSemesterHeader}
+                        setSemesterSelect={setSemesterSelect}
+                        semesterSelect={semesterSelect}
+                        SET_SEMESTER_MAP={SET_SEMESTER_MAP}
+                        SEMESTER_MAP={SEMESTER_MAP}
+                        SET_SAVE_BIN={SET_SAVE_BIN}
+                        SAVE_BIN={SAVE_BIN}
+                        binVisible={binVisible}
+                    ></Board> } 
+                </div>
+
                 
                 { visibleView === "3" && <DegreePlan
                     SET_SEMESTER_MAP={SET_SEMESTER_MAP}
@@ -66,22 +123,26 @@ function MainPage({ visibleView, setVisibleView }: {
                     setSemesterSelect={setSemesterSelect}
                     setSemesterHeader={setSemesterHeader}
                 ></DegreePlan> }
+                
 
                 { visibleView === "4" && <Calender></Calender> }
 
                 
             </section>
             <section className="cell-right">
-                { (visibleView === "2" || visibleView === "3") &&  <DisplayCourses
-                    SET_SEMESTER_MAP={SET_SEMESTER_MAP}
-                    SEMESTER_MAP={SEMESTER_MAP}
-                    semesterSelect={semesterSelect}
-                    setBinVisible={setBinVisible}
-                    binVisible={binVisible}
-                    SET_SAVE_BIN={SET_SAVE_BIN}
-                    SAVE_BIN={SAVE_BIN}
-                ></DisplayCourses> }            
+                <div className="right-menu-step">
+                    { (visibleView === "2" || visibleView === "3") &&  <DisplayCourses
+                        SET_SEMESTER_MAP={SET_SEMESTER_MAP}
+                        SEMESTER_MAP={SEMESTER_MAP}
+                        semesterSelect={semesterSelect}
+                        setBinVisible={setBinVisible}
+                        binVisible={binVisible}
+                        SET_SAVE_BIN={SET_SAVE_BIN}
+                        SAVE_BIN={SAVE_BIN}
+                    ></DisplayCourses> }            
+                </div>
             </section>
+            
 
             <SaveBin
                 setBinVisible={setBinVisible}
---
+++ src/components/WalkthroughButton.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function WalkthroughButton(): JSX.Element {
+    return(
+        <button>Walkthrough</button>
+    );
+}
